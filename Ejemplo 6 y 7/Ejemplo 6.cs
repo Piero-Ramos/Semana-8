838eaddbb7990a405af8a9ded109d893ab7757c6
Zeta-w <banosbruce9@gmail.com>
2025-10-07 09:45:22 -0500

    Estos son mis codigos que hice

---
+++ Ejemplo 6 y 7/Ejemplo 6.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        // Variables globales
+        static int num1;
+        static int num2;
+        static int resultado;
+
+        // Método para ingresar los números
+        static void Ingresar()
+        {
+            Console.Write("Ingrese el primer número natural: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo número natural: ");
+            num2 = int.Parse(Console.ReadLine());
+        }
+
+        // Método para sumar los números
+        static void Sumar()
+        {
+            resultado = num1 + num2;
+        }
+
+        // Método para imprimir el resultado
+        static void Imprimir()
+        {
+            Console.WriteLine("La suma de {0} + {1} es: {2}", num1, num2, resultado);
+        }
+
+        static void Main(string[] args)
+        {
+            Ingresar();
+            Sumar();
+            Imprimir();
+
+            Console.WriteLine("\nPresione cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejemplo 6 y 7/Ejemplo 7.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        // Método con paso por valor
+        static void incremento1(int n1)
+        {
+            n1 = n1 + 10;
+        }
+
+        // Método con paso por referencia
+        static void incremento2(ref int n1)
+        {
+            n1 = n1 + 10;
+        }
+
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 10;
+
+            incremento1(a);       // paso por valor
+            incremento2(ref b);   // paso por referencia
+
+            Console.WriteLine("a = " + a); // no cambia
+            Console.WriteLine("b = " + b); // cambia
+
+            Console.ReadKey();
+        }
+    }
+}
